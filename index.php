f5764f711b9cef8773306abc6f58ae2850a8678a
upsk-rc <150073782+upsk-rc@users.noreply.github.com>
2024-03-06 16:50:21 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
-<?php
+<img src="bacony_logo.jpg" alt="Bacony Logo">
 
-echo "Hello World!";
+<p>At Bacony, we pride ourselves on providing high-quality, delicious bacon products. From classic bacon strips to gourmet bacon flavors, we have something for every bacon lover.</p>
+
+<h2>Our Products:</h2>
+<ul>
+    <li>Classic Bacon Strips</li>
+    <li>Maple Bacon</li>
+    <li>Honey Sriracha Bacon</li>
+    <li>Peppered Bacon</li>
+    <li>Applewood Smoked Bacon</li>
+</ul>
+
+<h2>Contact Us:</h2>
+<p>If you have any questions or would like to place an order, please contact us at <a href="mailto:info@bacony.com">info@bacony.com</a> or give us a call at 1-800-BACONY.</p>
