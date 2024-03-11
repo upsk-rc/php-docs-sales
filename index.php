3736a54e459a79796f89ab9f30730c4e9a53554d
upsk-rc <150073782+upsk-rc@users.noreply.github.com>
2024-03-11 17:42:28 +0000

    Update index.php images test

--- index.php
+++ index.php
@@ -12,9 +12,9 @@
             <p><font color="#FFFFFF">Welcome to Bacon Co. We've got the best bacon in town!</font></p>
             <p><font color="#FFFFFF">Check out our products below:</font></p>
             <ul>
-                <li><font color="#FFFFFF">Bacon Strips</font><img src="baconstrip.gif" alt="Bacon Strip"></li>
-                <li><font color="#FFFFFF">Bacon Bits</font><img src="baconbit.gif" alt="Bacon Bit"></li>
-                <li><font color="#FFFFFF">Bacon Jam</font><img src="baconjam.gif" alt="Bacon Jam"></li>
+                <li><font color="#FFFFFF">Bacon Strips</font><img src="https://github.com/upsk-rc/php-docs-sales/blob/d7099ff33c5108407099edfd0baadf121e72bbb4/826a4104-9054-401c-95fa-a3fe591caa10.jpeg" alt="Bacon Strip"></li>
+                <li><font color="#FFFFFF">Bacon Bits</font><img src="https://github.com/upsk-rc/php-docs-sales/blob/d7099ff33c5108407099edfd0baadf121e72bbb4/826a4104-9054-401c-95fa-a3fe591caa10.jpeg" alt="Bacon Bit"></li>
+                <li><font color="#FFFFFF">Bacon Jam</font><img src="https://github.com/upsk-rc/php-docs-sales/blob/d7099ff33c5108407099edfd0baadf121e72bbb4/826a4104-9054-401c-95fa-a3fe591caa10.jpeg" alt="Bacon Jam"></li>
             </ul>
             <p><font color="#FFFFFF">Contact us:</font></p>
             <p><font color="#FFFFFF">Email: baconco@gmail.com</p>
