d7099ff33c5108407099edfd0baadf121e72bbb4
upsk-rc <150073782+upsk-rc@users.noreply.github.com>
2024-03-11 17:37:23 +0000

    Update index.php to 1998 with image placeholders

--- index.php
+++ index.php
@@ -4,22 +4,25 @@
     <title>Welcome to Bacon Co.</title>
 </head>
 <body>
-    <h1><font color="#FF0000">Bacon Co.</font></h1>
-    <p>Welcome to Bacon Co. We are your one-stop shop for all things bacon!</p>
-
-    <h2>Our Products:</h2>
-    <ul>
-        <li>Bacon Strips</li>
-        <li>Bacon Bits</li>
-        <li>Bacon Jam</li>
-    </ul>
-
-    <h2>Contact Us:</h2>
-    <p>Email: baconco@gmail.com</p>
-    <p>Phone: 555-555-5555</p>
-
-    <h2>Follow Us:</h2>
-    <p>Facebook: <a href="https://www.facebook.com/baconco">Bacon Co. Facebook</a></p>
-    <p>Twitter: <a href="https://twitter.com/baconco">Bacon Co. Twitter</a></p>
+    <center>
+        <h1><font color="#FFFF00">Bacon Co.</font></h1>
+    </center>
+    <hr>
+        <font size="4">
+            <p><font color="#FFFFFF">Welcome to Bacon Co. We've got the best bacon in town!</font></p>
+            <p><font color="#FFFFFF">Check out our products below:</font></p>
+            <ul>
+                <li><font color="#FFFFFF">Bacon Strips</font><img src="baconstrip.gif" alt="Bacon Strip"></li>
+                <li><font color="#FFFFFF">Bacon Bits</font><img src="baconbit.gif" alt="Bacon Bit"></li>
+                <li><font color="#FFFFFF">Bacon Jam</font><img src="baconjam.gif" alt="Bacon Jam"></li>
+            </ul>
+            <p><font color="#FFFFFF">Contact us:</font></p>
+            <p><font color="#FFFFFF">Email: baconco@gmail.com</p>
+            <p><font color="#FFFFFF">Phone: 555-555-5555</p>
+            <p><font color="#FFFFFF">Follow us:</p>
+            <p><a href="https://www.facebook.com/baconco"><font color="#FFFFFF">Facebook</a></font></p>
+            <p><a href="https://twitter.com/baconco"><font color="#FFFFFF">Twitter</a></font></p>
+        </font>
+    <hr>
 </body>
 </html>
