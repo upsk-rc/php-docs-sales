d4233e2b0c96f23185818fce74e4a9982097a7e1
upsk-rc <150073782+upsk-rc@users.noreply.github.com>
2024-03-11 17:59:09 +0000

    Update index.php early 1990s

--- index.php
+++ index.php
@@ -1,28 +1,39 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>Welcome to Bacon Co.</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bacony - Bringing Home the Sizzle!</title>
+    <style>
+        /* Basic 90s-style CSS */
+        body {
+            background-color: #000;
+            color: #fff;
+            font-family: Arial, sans-serif;
+            text-align: center;
+            margin: 0;
+            padding: 0;
+        }
+        h1 {
+            font-size: 24px;
+            margin-top: 50px;
+        }
+        p {
+            font-size: 16px;
+        }
+        .bacon-logo {
+            width: 150px;
+            height: auto;
+        }
+    </style>
 </head>
 <body>
-    <center>
-        <h1><font color="#FFFF00">Bacon Co.</font></h1>
-    </center>
-    <hr>
-        <font size="4">
-            <p><font color="#FFFFFF">Welcome to Bacon Co. We've got the best bacon in town!</font></p>
-            <p><font color="#FFFFFF">Check out our products below:</font></p>
-            <ul>
-                <li><font color="#FFFFFF">Bacon Strips</font><img src="https://github.com/upsk-rc/php-docs-sales/blob/d7099ff33c5108407099edfd0baadf121e72bbb4/826a4104-9054-401c-95fa-a3fe591caa10.jpeg" alt="Bacon Strip"></li>
-                <li><font color="#FFFFFF">Bacon Bits</font><img src="https://github.com/upsk-rc/php-docs-sales/blob/d7099ff33c5108407099edfd0baadf121e72bbb4/826a4104-9054-401c-95fa-a3fe591caa10.jpeg" alt="Bacon Bit"></li>
-                <li><font color="#FFFFFF">Bacon Jam</font><img src="https://github.com/upsk-rc/php-docs-sales/blob/d7099ff33c5108407099edfd0baadf121e72bbb4/826a4104-9054-401c-95fa-a3fe591caa10.jpeg" alt="Bacon Jam"></li>
-            </ul>
-            <p><font color="#FFFFFF">Contact us:</font></p>
-            <p><font color="#FFFFFF">Email: baconco@gmail.com</p>
-            <p><font color="#FFFFFF">Phone: 555-555-5555</p>
-            <p><font color="#FFFFFF">Follow us:</p>
-            <p><a href="https://www.facebook.com/baconco"><font color="#FFFFFF">Facebook</a></font></p>
-            <p><a href="https://twitter.com/baconco"><font color="#FFFFFF">Twitter</a></font></p>
-        </font>
-    <hr>
+    <h1>Welcome to Bacony!</h1>
+    <p>Bringing you the crispiest bacon since 1990.</p>
+    <p>Our secret recipe will make your taste buds dance!</p>
+    <p>Visit our <a href="menu.php">menu</a> for mouthwatering bacon delights.</p>
+    <footer>
+        © 1990-2024 Bacony. All rights reserved.
+    </footer>
 </body>
 </html>
